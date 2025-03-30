f2882135601cdfb8812766ad62ae6b7860af5365
sohnorman3 <118236031+sohnorman3@users.noreply.github.com>
2025-03-30 21:14:51 +0800

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,28 @@
+def find_and_replace(lst, find_val, replace_val):
+    """
+    Task 1
+    - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
+    - lst must be a list.
+    - Return the modified list.
+    """
+    if not isinstance(lst, list): #checking isinstance compare if object lst is a list. "Not" function compare if the false run next line.
+        raise ValueError("Input must be a list")  #raise is an exception handling and Value error define the incorrect format.
+
+    return [replace_val if item == find_val else item for item in lst]  #  check each item in list with for item in lst, if item is match exactly, replace the find_val with replace_val
+
+
+
+# Task 2
+# Invoke the function "find_and_replace" using the following scenarios:
+# - [1, 2, 3, 4, 2, 2], 2, 5
+# - ["apple", "banana", "apple"], "apple", "orange"
+
+# Test Case 1: Replacing 2s with 5s
+list1 = [1, 2, 3, 4, 2, 2]
+result1 = find_and_replace(list1, 2, 5) # replace all 2 with the value 5 in all the value in the list
+print(result1)  # Output: [1, 5, 3, 4, 5, 5]
+
+# Test Case 2: Replacing "apple" with "orange"
+list2 = ["apple", "banana", "apple"]
+result2 = find_and_replace(list2, "apple", "orange") #replace all apple with Orange in the list. This does not change current orange in the list. 
+print(result2)  # Output: ["orange", "banana", "orange"]
---
+++ q4.py
@@ -0,0 +1,34 @@
+def string_reverse(s):
+    """
+    Task 1
+    - Create a function that reverses a given string (s).
+    - s must be a string.
+    - Return the reversed string.
+    """
+
+    def string_reverse(s):
+        """
+        Task 1
+        - Create a function that reverses a given string (s).
+        - s must be a string.
+        - Return the reversed string.
+        """
+        if not isinstance(s, str): #check if s is a str and if false, run the next line.
+            raise ValueError("Input must be a string") # exception raise for input error for the requirement.
+
+    return s[::-1]  # Reverse the string using slicing. [Start,end, step] with [::-1] we are defining default all beginning and default ending with a negative 1 left to right order.
+
+
+
+# Task 2
+# Invoke the function "string_reverse" using the following scenarios:
+# - "Hello World"
+# - "Python"
+
+# Test Case 1: Reversing "Hello World"
+result1 = string_reverse("Hello World") #define object result1, call function string reverse
+print(result1)    # print result of result1
+
+# Test Case 2: Reversing "Python"
+result2 = string_reverse("Python")   #define object result2, call function string reverse
+print(result2)  #  Print result of result2
