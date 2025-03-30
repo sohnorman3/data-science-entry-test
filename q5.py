1680f06aaab3fd0cd3d23949e99723085e078440
sohnorman3 <118236031+sohnorman3@users.noreply.github.com>
2025-03-30 21:51:20 +0800

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,20 @@
+def check_divisibility(num, divisor):
+    """
+    Task 1
+    - Create a function to check if the number (num) is divisible by another number (divisor).
+    - Both num and divisor must be numeric.
+    - Return True if num is divisible by divisor, False otherwise.
+    """
+    if not (isinstance(num, (int, float)) and isinstance(divisor, (int, float))): # check if num is a numeric and Divisor is numeric. if either is false run next line
+        raise ValueError("Both num and divisor must be numeric.") # exception handing of message both num and divisor must be numeric
+
+    return num % divisor == 0  # Return True if num is divisible by divisor. alternatively using return (num / divisor).is_integer() or non decimal
+
+
+
+# Task 2
+# Invoke the function "check_divisibility" using the following scenarios:
+# - 10, 2
+# - 7, 3
+print(check_divisibility(10, 2))  # Expected Output: True, because 10 is divisible by 2
+print(check_divisibility(7, 3))   # Expected Output: False, because 7 is not divisible by 3
